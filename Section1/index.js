64345e7cf75be161e67bae1ed713e537227b0a53
phanminhtruc <pmtruc2012@gmail.com>
2021-10-01 21:47:49 +0700

    add section1

---
+++ Section1/index.js
@@ -0,0 +1,84 @@
+BT 1:
+ Part 1
+In JavaScript, I will get the SyntaxError when:
+1. Missing or  being redundant syntax.
+
+Example: 
+var = one    // without the syntax
+
+2. Declaring mutiple variables at the same time:
+Example:
+var x,y = "ten",
+var x, = "ten"
+3. Missing a variable name:
+Example:
+var = "ho";
+Part 2:
+In Javascript, we can check a variable data type by:
+1. Checking for undenfined
+Example:
+var a;
+if(typeof a== "undefined"
+   console.log("undefined value");
+else
+   console.log("defined valure");
+2. Checking for null:
+Example:
+var b = null;
+if(b === null)
+    console.log("null value");
+else
+    console.log("not null value");
+3. Checking for a number:
+Example:
+var a =5;
+if("typeof a == "number")
+    console.log("numeric value");
+else
+    console.log("not a number");
+
+
+BT 2:
+My answer:
+1. A
+2. B
+3. C
+4. E
+
+BT 3:
+Code:
+A.
+var message = "code is great";
+console.log(message);
+B.
+var studentCount = 0;
+studentCount = 16;
+console.log(studentCount);
+
+BT 4:
+A.
+var str = "coding might not be easy, but till great";
+console.log(str);
+
+B.
+var studentCount = 0;
+studentCount = 16;
+console.log(studentCount);
+
+C.
+var str = "coding might not be easy, but still great";
+str = "coding might not be easy, but still great";
+console.log(str.toLowerCase());
+
+D.
+var studentCount = 0;
+studentCount = 16;
+studentCount + = 1;
+console.log(studentCount);
+
+BT 5:
+alert ('You look beautiful today');
+
+BT 6, BT 7, BT 8
+
+Mình chưa học đến phần này và cũng chưa nắm được cách tạo box trong JS nên xin phép được để sau.
